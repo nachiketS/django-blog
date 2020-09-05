15aeda24ddfefed7f563fb0aaebe3504c46a0342
nachiketS <nachiket3598@gmail.com>
2020-09-05 15:31:20 +0530

    added django-heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -132,3 +133,5 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = "blog-home"
 LOGIN_URL = "users-login"
+
+django_heroku.settings(locals())
